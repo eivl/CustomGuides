555a449056910246f73de3b44740a551c463e563
Ozzi <mark.parr20@gmail.com>
2022-06-20 04:25:52 +0930

    Create NPCs_enUS.lua

---
+++ DugisGuideViewerZ_Custom_Guides/Localization/NPCs_enUS.lua
@@ -0,0 +1,34 @@
+local DGV = DugisGuideViewer
+if not DGV then return end
+local NPC = DGV:RegisterModule("NPC")
+function NPC:ShouldLoad()
+	return DGV:UserSetting(DGV_ENABLENPCNAMEDB) 
+		and (DugisGuideViewer.GuideOn()	or not DGV:UserSetting(DGV_UNLOADMODULES))
+end
+function NPC:Initialize()
+	function NPC:Load()
+		DugisNPCsEn2 = {
+
+}
+		if DugisNPCsEn then 
+				for k, v in pairs(DugisNPCsEn2) do
+						if type(v) == 'table' then
+								DugisNPCsEn[k] = merge(DugisNPCsEn[k], DugisNPCsEn2[k])
+						else
+							DugisNPCsEn[k] = v
+						end
+				end
+		end
+
+	end
+
+	function NPC:Unload()
+	   wipe(DugisNPCsEn2)
+	end
+
+	function NPC:OnModulesLoaded()
+	   NPC.Initialize = nil
+	   NPC.Load = nil
+	   NPC.initialized = false
+	end
+end
